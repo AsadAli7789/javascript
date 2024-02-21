2432b148007b5b52412349e73040bbc96c000cdf
AsadAli7789 <asadali428killer@gmail.com>
2024-02-21 14:09:14 -0800

    done

---
+++ app.js
@@ -0,0 +1,48 @@
+// Q1 write the script to greet your website visitor  
+var greet="hello!"
+alert (greet)
+// Q2 write a script to display folling message on your website 
+var pass="Error! please enter the valid password"
+alert(pass) 
+// Q3 write a script to display folling message on your website ( hint use line break )
+var brek ="welcome to js land \n happy coding "
+// Q4 write a script to display folling message in sequence
+var los ="Welcome to js land "
+alert(los) 
+
+var sui="happy coding \n prevent this page from creating additional dialogs"
+alert(sui) 
+
+
+
+
+
+// chapter 2
+// Q1
+var sfi="Syed Muhammad Asad Ali"
+alert(sfi) 
+// Q2
+var sdb="18 year old "
+alert(sdb) 
+// Q3
+var sdb="example@gmail.com "
+alert(sdb) 
+// Q5
+var sdb="Pizza \n Pizz \n Piz \n Pi \n P"
+alert(sdb) 
+
+
+// Q6
+
+var y=" example@gmail.com "
+var x="my email address is"
+var z=x+y
+alert(z) 
+
+
+
+
+var book= "smater way to learn java script "
+var first = "i am trying to learn from this book "
+var last= first+book
+alert (last)
