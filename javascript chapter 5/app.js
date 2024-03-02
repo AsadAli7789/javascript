2ba82123af91281d80a7847846f7f4747e305c8c
AsadAli7789 <asadali428killer@gmail.com>
2024-03-01 17:03:24 -0800

    sone

---
+++ javascript chapter 5/app.js
@@ -0,0 +1,56 @@
+// adds two numbers together and returns the result.
+var a=80 , b=50 
+var adds = a+b
+document.getElementById("asad").textContent="a=80 , b=50 ";
+document.getElementById("asa").textContent="a+b="+adds;
+
+// subtract one number from another and returns the result
+var a=80 , b=50 
+var substract = a-b
+document.getElementById("as").textContent="a=80 , b=50 ";
+document.getElementById("a").textContent="a-b="+substract;
+
+
+
+
+
+
+
+
+
+// multiply two numbers and returns the product
+var a=80 , b=50 
+var multiply = a*b
+document.getElementById("asd").textContent="a=80 , b=50 ";
+document.getElementById("ad").textContent="a*b="+multiply;
+
+
+
+
+var a=80 , b=50 
+var divide = a/b
+document.getElementById("lo").textContent="a=80 , b=50 ";
+document.getElementById("pol").textContent="a/b="+ divide;
+
+
+
+
+
+
+
+
+var math=80 , physic=45 , chemistry=42 , urdu=78 ,  english=84
+var totalmarks = math+physic+chemistry+urdu+english
+var percentage = totalmarks/450
+var percentage2= percentage*100
+
+document.getElementById("opl").textContent="math=80 physic=45 chemistry=42 urdu=78 english=84 ";
+document.getElementById("dcg").textContent="="+percentage2;
+
+
+
+
+var ticketPrice=600
+var numberOfTicket=5
+var price =ticketPrice*numberOfTicket
+document.getElementById("lgp").textContent=" ticket=600 number=5 cost="+price;
