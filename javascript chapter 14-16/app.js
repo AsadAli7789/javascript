6c8735555cf1e9e48f6fc0341d866507e8cfd4ea
AsadAli7789 <asadali428killer@gmail.com>
2024-03-13 16:32:23 -0700

    done

---
+++ javascript chapter 14-16/app.js
@@ -0,0 +1,43 @@
+
+// Create an empty array and use the push method to add five different elements to it.
+var aryy = []
+console.log(aryy)
+aryy.push("karachi")
+console.log(aryy)
+aryy.push("lahore")
+console.log(aryy)
+aryy.push("pewshawar")
+console.log(aryy)
+aryy.push("Islamabad")
+console.log(aryy)
+aryy.push("pindi")
+console.log(aryy)
+
+
+// Use the pop method to remove the last element from the array.
+aryy.pop()
+console.log(aryy)
+// Use the shift method to remove the first element from the array.
+aryy.shift()
+console.log(aryy)
+
+// Use the unshift method to add a new element at the beginning of the array.
+aryy.unshift("landi")
+console.log(aryy)
+// Print the final array after each operation to observe the changes.
+
+
+
+
+
+
+
+
+// Create an array with at least 5 elements.
+var arraay = ["apple", "banana","mango","pineapple","watermelon"]
+console.log(arraay)
+// Use the length property to find the number of elements in the array.
+console.log(arraay.length);
+// Access and print the first and last elements of the array using their indices.
+console.log(arraay[0])
+console.log(arraay[arraay.length-1])
